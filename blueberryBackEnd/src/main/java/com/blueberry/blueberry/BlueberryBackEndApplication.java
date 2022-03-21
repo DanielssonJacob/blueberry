07485b57ea7034099437361eb9e1d5a32459e9fe
Jacob Danielsson <danielsson_jacob@yahoo.com>
2022-03-21 15:13:26 +0100

    blueberry back-end spring boot service

---
+++ blueberryBackEnd/src/main/java/com/blueberry/blueberry/BlueberryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.blueberry.blueberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueberryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueberryBackEndApplication.class, args);
+	}
+
+}
---
+++ blueberryBackEnd/src/test/java/com/blueberry/blueberry/BlueberryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueberry.blueberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueberryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
