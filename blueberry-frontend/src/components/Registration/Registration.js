de6ad8b385c63436aca1eb33b14cb283b162a5de
potemkin <tom.alpsten@gmail.com>
2022-03-22 15:19:55 +0100

    CSS updates to registration-page

--- blueberry-frontend/src/components/Registration/Registration.js
+++ blueberry-frontend/src/components/Registration/Registration.js
@@ -5,27 +5,45 @@ import {
     Route,
     Link
   } from "react-router-dom";
+  import DefaultButton from '../default/DefaultButton';
+  import './Registration.css'
 
 function Registration() {
   return (
     <div>
-        <div className="header">
-            <div className="logo">
-            <h2>Company Registration</h2>
+        <div className="header1">
+            <div className="title">
+            <h1>Company Registration</h1>
         
         </div>
       </div>
-      <div className="Company information input">
-          <label for="cname">Company name:</label><br></br>
-          <input type="text" id="cname" name="cname"></input><br></br>
-          <label for="address">Address:</label><br></br>
-          <input type="text" id="caddress" name="cadress"></input><br></br>
-          <label for="cperson">Contact person:</label><br></br>
-          <input type="text" id="cperson" name="cperson" ></input><br></br>
-          <label for="cdescription">Company description:</label><br></br>
-          <input type="textarea" id="cdescription" name="cdescription"></input><br></br>
-          <label for="openinghours">Opening hours:</label><br></br>
-          <input type="text" id="openinghours" name="openinghours"></input><br></br>
+      <div className="logo1">
+        
+      </div>
+      <div className="companyInformationInput">
+          <div className="inputField">
+            <label for="cname">Company name:</label><br></br>
+            <input type="text" id="cname" name="cname"></input><br></br>
+          </div>
+          <div className="inputField">
+            <label for="address">Address:</label><br></br>
+            <input type="text" id="caddress" name="cadress"></input><br></br>
+          </div>
+          <div className="inputField">
+            <label for="cperson">Contact person:</label><br></br>
+            <input type="text" id="cperson" name="cperson" ></input><br></br>
+          </div>
+          <div className="inputField">
+            <label for="cdescription">Company description:</label><br></br>
+            <textarea type="textarea" id="cdescription" name="cdescription" rows="4" cols="50"></textarea><br></br>
+          </div>
+          <div className="inputField">
+            <label for="openinghours">Opening hours:</label><br></br>
+            <input type="text" id="openinghours" name="openinghours"></input><br></br>
+          </div>
+      </div>
+      <div className="registerButton">
+        <DefaultButton link="/individual" title="Register"/>
       </div>
         
         <Link to="/">Home</Link>
