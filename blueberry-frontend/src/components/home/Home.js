4a5f5c936e29cc19a85ea165ff618b1e1a6838b1
Jacob Danielsson <danielsson_jacob@yahoo.com>
2022-03-22 11:22:56 +0100

    added home styling and home button

--- blueberry-frontend/src/components/home/Home.js
+++ blueberry-frontend/src/components/home/Home.js
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import React from 'react'
 import {
   BrowserRouter as Router,
@@ -6,6 +7,9 @@ import {
   Link
 } from "react-router-dom";
 import "./Home.css"
+import HomePageButton from './HomePageButton';
+import blueberry from './blueberry-img.png';
+import ColorRow from './ColorRow';
 
 function Home() {
   return (
@@ -18,15 +22,19 @@ function Home() {
       </div>
 
       <div className="menu-choice">
-        <div className="menu-item">
-          <h2>Individual</h2>
-        </div>
-        <div className="menu-item">
-          <h2>Organization</h2>
-        </div>
-        <div className="menu-item">Image</div>
+      <div className="menu-item">
+        <HomePageButton link="/individual" title="Individual"/>
+      </div> 
+    
+      <div className="menu-item">
+        <HomePageButton link="/organization" title="Organization"/>
+      </div> 
+        <div className="menu-item"><img src={blueberry} alt="" /></div>
       </div>
-
+      <div className='divider'></div>
+      <ColorRow color={"#E8340C"} height={"60px"}></ColorRow>
+      <ColorRow color={"#F5FA70"} height={"60px"}></ColorRow>
+      <ColorRow color={"#0271BB"} height={"60px"}></ColorRow>
     </div>
   )
 }
