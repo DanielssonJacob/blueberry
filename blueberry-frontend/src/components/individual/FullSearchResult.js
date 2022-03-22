d58ac5ab9b30517cf086681d39faeb011f0feb90
johanna <johannautter@gmail.com>
2022-03-22 13:27:19 +0100

    fix article-size

--- blueberry-frontend/src/components/individual/FullSearchResult.js
+++ blueberry-frontend/src/components/individual/FullSearchResult.js
@@ -24,7 +24,9 @@ function FullSearchResult() {
                 <div className='companyImage'>
                     <img src={redcross} alt="red cross" />
                 </div>
-                <div className='companyInfo'>Lorem Ipsum</div>
+                <div className='companyInfo'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Vivamus ac ex augue. Duis bibendum ornare arcu, sed congue enim fermentum ut. Aliquam 
+                vestibulum laoreet nibh. ae volutpat...</div>
             </article>
             <article>
                 <div className='companyName'>Name</div>
