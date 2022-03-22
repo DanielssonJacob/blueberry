cc69c4f372a73ff16e3b4e17afdf225dc2e2f0d6
frykj <fryk89@gmail.com>
2022-03-22 09:37:35 +0100

    companyDetails v1.0

---
+++ blueberry-frontend/src/components/Company-details/companyDetails.js
@@ -0,0 +1,19 @@
+    import React from 'react'
+    import {
+        BrowserRouter as Router,
+        Switch,
+        Route,
+        Link
+      } from "react-router-dom";
+    
+    function companyDetails() {
+      return (
+        <div>companyDetails
+            company information
+        </div>
+
+      )
+    }
+    
+    
+    export default companyDetails
