a41f3a4528d0b351f8f5094521e4079b213e3b3d
Vanja Jonasson <vanja_j@hotmail.com>
2022-03-22 11:43:12 +0100

    login page changed

--- blueberry-frontend/src/components/organization/Organization.js
+++ blueberry-frontend/src/components/organization/Organization.js
@@ -1,26 +1,62 @@
 import React from 'react'
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-  import './Organization.css'
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './Organization.css'
 
 function Organization() {
   return (
-    <div className="org-menu">
-        <div className="sign-in">
-        <p>Sign in</p>
+    <div className="org-main">
+      <div className="sub-main">
+
+        <div>
+          <h1>Login</h1>
+          <div>
+            <input type="email" placeholder="username" className="input" />
+          </div>
+
+          <div>
+            <input type="password" placeholder="password" className="input" />
+          </div>
+
+
+          <button >Sign in</button>
+
+          <div>
+          <a className="link" href="">Create account</a>
+          </div>
+
+
         </div>
-        <div className="create-acc">
-        <p>Create account</p>
-        </div>
+
+
+      </div>
     </div>
 
 
-    
+
   )
 }
+
+/*
+function Organization() {
+return (
+  <div className="org-menu">
+      <div className="sign-in">
+      <p>Sign in</p>
+      </div>
+      <div className="create-acc">
+      <p>Create account</p>
+      </div>
+  </div>
+
+
+  
+)
+}
+*/
 
 export default Organization
