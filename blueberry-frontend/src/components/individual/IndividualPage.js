1760056d13c69edd11f164b60f3173dc04c7447c
Jacob Danielsson <danielsson_jacob@yahoo.com>
2022-03-23 16:54:23 +0100

    fix: some error handling depending on input

--- blueberry-frontend/src/components/individual/IndividualPage.js
+++ blueberry-frontend/src/components/individual/IndividualPage.js
@@ -8,11 +8,15 @@ import Autocomplete from '@mui/material/Autocomplete';
 import useFetch from "react-fetch-hook";
 import { useState } from 'react'
 import { useHistory } from "react-router-dom";
+import Alert from '@mui/material/Alert';
 
 function IndividualPage() {
     const [ organization, setOrganization ] = useState("")
 
     const { isLoading, data, error } = useFetch("http://localhost:8080/companies");
+
+    const [isAlert, setIsAlert] = useState(false);
+    const [snapshot, setSnapshot] = useState("");
 
     let history = useHistory();
 
@@ -24,23 +28,46 @@ function IndividualPage() {
     }
     return (
         <div className="individual-page-body">
+        {organization===""?<Alert hidden={!isAlert} severity="error">Error: You need to specify which organization you are looking for.</Alert>:<Alert hidden={!isAlert} severity="error">Error: Can't find {snapshot}.</Alert>}
+        
             <div className="individual-page-header">
                 <Logo></Logo>
                 <SignInField link="/"></SignInField>
             </div>
             <div className="individual-page-searchfield">
                 <div className="company-search-individual">
-                    <label htmlFor='company-search-input'>Organization</label>
-                    <input id="company-search-input" type="text" className="form-control" onChange={(e)=>setOrganization(e.target.value)}
-                    onKeyPress={(e) => {
-                        if (e.key === "Enter") {
-                            history.push(`/company/${organization}`)
-                        }
-                    }} />
+                    <label htmlFor='company-search-input'></label>
+                    <Autocomplete
+                        freeSolo
+                        id="company-search-input"
+                        className='form-control company-search-individual'
+                        inputValue={organization}
+                        onInputChange={(event, newInputValue) => {
+                            setOrganization(newInputValue);
+                        }}
+                        options={data.map((c => c.name))}
+                        sx={{height: "70px" , backgroundColor: "white" }}
+                        renderInput={(params) => <TextField {...params} label="Organizations" />}
+                        onKeyDown={(event) => {
+                            if (event.key === 'Enter') {
+                                // Prevent's default 'Enter' behavior.
+                                event.defaultMuiPrevented = true;
+                                // your handler code
+                                if(data.find(c => c.name===organization)!=null){
+                                    setIsAlert(false)
+                                    history.push(`/company/${organization}`)
+                                } else{
+                                    setSnapshot(organization)
+                                    setIsAlert(true)
+                                }
+                                
+                            }
+                        }}/>
+
                 </div>
                 <div className="company-dropdown-individual">
                     <label htmlFor="company-search-select">Action</label>
-                    <select id="company-search-select" className="form-select donation-selection" aria-label="Default select example">
+                    <select id="company-search-select" className="form-select donation-selection">
                         <option selected>Donate</option>
                         <option>Volunteer</option>
                         <option>Donation Pickup</option>
