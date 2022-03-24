cd1a79956e06733e83a2688043e404369c65c6bf
potemkin <tom.alpsten@gmail.com>
2022-03-24 12:34:08 +0100

    Added some CSS-stuff to the registration page

--- blueberry-frontend/src/components/Registration/Registration.js
+++ blueberry-frontend/src/components/Registration/Registration.js
@@ -7,6 +7,7 @@ import {
   Link
 } from "react-router-dom";
 import DefaultButton from '../default/DefaultButton';
+import Logo from '../default/Logo';
 import './Registration.css'
 import { useState } from "react";
 import Alert from '@mui/material/Alert';
@@ -105,8 +106,12 @@ function Registration() {
   }  */
   return (
    // <form onSubmit={handleSubmit}>
-      <div>
+      <body className="body">
         <Link to="/">Home</Link>
+        <div className="logo1">
+            <Logo></Logo>
+        </div>
+        
         <Alert hidden={!isAlert} severity="error">{errorMessage}</Alert>
         <div className="header1">
           <div className="title">
@@ -115,9 +120,7 @@ function Registration() {
 
           </div>
         </div>
-        <div className="logo1">
-
-        </div>
+        
         <div className="companyInformationInput">
           <div className="inputField">
             <label for="cName">Company name: </label>
@@ -156,7 +159,7 @@ function Registration() {
         
         
         
-      </div>
+      </body>
     //</form>
   )
 }
