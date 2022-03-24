967fa18e3a7fdd921a507fc677013596229058d2
frykj <fryk89@gmail.com>
2022-03-24 16:49:33 +0100

    cleaning

--- blueberry-frontend/src/components/Company-details/CompanyDetails.js
+++ blueberry-frontend/src/components/Company-details/CompanyDetails.js
@@ -26,7 +26,6 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import useFetch from "react-fetch-hook";
 
-
 function createData(day, opening, closing) {
     return { day, opening, closing };
 }
@@ -45,120 +44,115 @@ const currentBread = "Right now we need your help getting more fresh water for t
 const currentCaption = "water for uganda"
 const googlemap = "GOOGLE MAP"
 
-
-
-
 function CompanyDetails() {
-    
+
     let { companyname } = useParams();
-    const { isLoading, data, error } = useFetch("http://localhost:8080/company/" + companyname); 
+    const { isLoading, data, error } = useFetch("http://localhost:8080/company/" + companyname);
     if (error) {
         return <h2>Error</h2>
     }
-    
+
     return (
 
         <div>
             {isLoading ? <h2>Loading...</h2> : data.map((c) =>
-            <div>
-            <div>
-            <h1 className='companyName' >{c.name}</h1>
-                <Box sx={{ flexGrow: 1 }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={4}>
-                            <Container maxWidth="sm">
-                                <img src={Image}></img>
-                            </Container>
-                        </Grid>
-                        <Grid item xs={4}>
-                           {c.description}
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Container maxWidth="sm">
-                                {googlemap}
-                            </Container>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Container maxWidth="sm">
-                                <Card sx={{ minWidth: 275 }}>
-                                    <CardContent>
-                                        Adress: {c.address} <br></br>Kontaktperson: {c.person}
-                                    </CardContent>
-                                </Card>
-                            </Container>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Container maxWidth="sm">
-                                <TableContainer component={Paper}>
-                                    <Table sx={{ minWidth: 50 }} aria-label="simple table">
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell>Day of the week</TableCell>
-                                                <TableCell align="right">Opening</TableCell>
-                                                <TableCell align="right">closing</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {rows.map((row) => (
-                                                <TableRow
-                                                    key={row.day}
-                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                                    <TableCell component="th" scope="row">
-                                                        {row.day}
-                                                    </TableCell>
-                                                    <TableCell align="right">{row.opening}</TableCell>
-                                                    <TableCell align="right">{row.closing}</TableCell>
-                                                </TableRow>
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
-                            </Container>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Container maxWidth="sm">
-                                <Card sx={{ minWidth: 275 }}>
-                                    <CardContent>
-                                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                            Current
-                                        </Typography>
-                                        <Typography sx={{ mb: 2.5 }} variant="h5" component="div">
-                                            {currentCaption}
-                                        </Typography>
-                                        <Typography variant="body2">
-                                            {currentBread}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small">Learn More</Button>
-                                    </CardActions>
-                                </Card>
-                            </Container>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </div>
-           </div>
-           )}
-
+                <div>
+                    <div>
+                        <h1 className='companyName' >{c.name}</h1>
+                        <Box sx={{ flexGrow: 1 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={4}>
+                                    <Container maxWidth="sm">
+                                        <img src={Image}></img>
+                                    </Container>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    {c.description}
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Container maxWidth="sm">
+                                        {googlemap}
+                                    </Container>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Container maxWidth="sm">
+                                        <Card sx={{ minWidth: 275 }}>
+                                            <CardContent>
+                                                Adress: {c.address} <br></br>Kontaktperson: {c.person}
+                                            </CardContent>
+                                        </Card>
+                                    </Container>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Container maxWidth="sm">
+                                        <TableContainer component={Paper}>
+                                            <Table sx={{ minWidth: 50 }} aria-label="simple table">
+                                                <TableHead>
+                                                    <TableRow>
+                                                        <TableCell>Day of the week</TableCell>
+                                                        <TableCell align="right">Opening</TableCell>
+                                                        <TableCell align="right">closing</TableCell>
+                                                    </TableRow>
+                                                </TableHead>
+                                                <TableBody>
+                                                    {rows.map((row) => (
+                                                        <TableRow
+                                                            key={row.day}
+                                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                                            <TableCell component="th" scope="row">
+                                                                {row.day}
+                                                            </TableCell>
+                                                            <TableCell align="right">{row.opening}</TableCell>
+                                                            <TableCell align="right">{row.closing}</TableCell>
+                                                        </TableRow>
+                                                    ))}
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>
+                                    </Container>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Container maxWidth="sm">
+                                        <Card sx={{ minWidth: 275 }}>
+                                            <CardContent>
+                                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                    Current
+                                                </Typography>
+                                                <Typography sx={{ mb: 2.5 }} variant="h5" component="div">
+                                                    {currentCaption}
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    {currentBread}
+                                                </Typography>
+                                            </CardContent>
+                                            <CardActions>
+                                                <Button size="small">Learn More</Button>
+                                            </CardActions>
+                                        </Card>
+                                    </Container>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </div>
+                </div>
+            )}
             <div className='blogheader'>
-            <Grid item xs={12}>
-                <Container className='blogpost'>
-                <Card>
-                    bla bla bla
-                </Card>
-                </Container>
-            </Grid>
-            <Grid item xs={12}>
-                <Container className='blogpost'>
-                <Card>
-                    bla bla bla
-                </Card>
-                </Container>
-            </Grid>
+                <Grid item xs={12}>
+                    <Container className='blogpost'>
+                        <Card>
+                            bla bla bla
+                        </Card>
+                    </Container>
+                </Grid>
+                <Grid item xs={12}>
+                    <Container className='blogpost'>
+                        <Card>
+                            bla bla bla
+                        </Card>
+                    </Container>
+                </Grid>
             </div>
         </div>
-
     )
 }
 
