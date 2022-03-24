599f2b05f0a16d7b2cd13930eb621640ad01c9f9
Jacob Danielsson <danielsson_jacob@yahoo.com>
2022-03-24 11:17:06 +0100

    fix: constructor

--- blueberryBackEnd/src/main/java/com/blueberry/blueberry/CompanyController.java
+++ blueberryBackEnd/src/main/java/com/blueberry/blueberry/CompanyController.java
@@ -19,7 +19,7 @@ public class CompanyController {
     List<Company> getCompanies(){
         if(companyRepository.findAll().size()<6){
             companyRepository.saveAll(Arrays.asList(
-                    new Company(null, "UNICEF", "Sankt Eriksgatan 46, 112 34 Stockholm","Stockholm" "Pernilla Baralt", "UNICEF arbetar för alla barns rättigheter. Över hela världen kämpar vi för att alla barn ska få den barndom som de har rätt till. Men vi behöver din hjälp.", "12 till 12"),
+                    new Company(null, "UNICEF", "Sankt Eriksgatan 46, 112 34 Stockholm","Stockholm", "Pernilla Baralt", "UNICEF arbetar för alla barns rättigheter. Över hela världen kämpar vi för att alla barn ska få den barndom som de har rätt till. Men vi behöver din hjälp.", "12 till 12"),
                     new Company(null, "Röda Korset", "Götavägen 22, 110 30 Stockholm","Stockholm", "Gunnar Skogqvist", "Vi är världens största humanitära nätverk och vi finns i 192 länder. Målet är att förhindra och lindra mänskligt lidande, oavsett vem det drabbar.", "12 till 12"),
                     new Company(null, "Amnesty International", "Haga Östergata 30, 411 22 Göteborg","Göteborg", "Ahmed Rakhtun", "Amnesty International är en global organisation som kämpar för alla människors lika värde. Vi arbetar för att mänskliga rättigheter ska gälla alla, nu och i framtiden.", "10 till 20"),
                     new Company(null, "Rädda barnen", "Gustavslundsvägen 141, 107 88 Stockholm","Stockholm", "Alia Bhatt", "Vi finns på plats i Sverige och 119 andra länder och arbetar för alla barns självklara rätt att överleva, utvecklas och växa upp i trygghet. Att rädda barnen är att rädda världen.", "09 till 15"),
@@ -39,7 +39,7 @@ public class CompanyController {
     List<Company> getCompany(@PathVariable String name){
         if(companyRepository.findAll().size()<6) {
             companyRepository.saveAll(Arrays.asList(
-                    new Company(null, "UNICEF", "Sankt Eriksgatan 46, 112 34 Stockholm","Stockholm" "Pernilla Baralt", "UNICEF arbetar för alla barns rättigheter. Över hela världen kämpar vi för att alla barn ska få den barndom som de har rätt till. Men vi behöver din hjälp.", "12 till 12"),
+                    new Company(null, "UNICEF", "Sankt Eriksgatan 46, 112 34 Stockholm","Stockholm", "Pernilla Baralt", "UNICEF arbetar för alla barns rättigheter. Över hela världen kämpar vi för att alla barn ska få den barndom som de har rätt till. Men vi behöver din hjälp.", "12 till 12"),
                     new Company(null, "Röda Korset", "Götavägen 22, 110 30 Stockholm","Stockholm", "Gunnar Skogqvist", "Vi är världens största humanitära nätverk och vi finns i 192 länder. Målet är att förhindra och lindra mänskligt lidande, oavsett vem det drabbar.", "12 till 12"),
                     new Company(null, "Amnesty International", "Haga Östergata 30, 411 22 Göteborg","Göteborg", "Ahmed Rakhtun", "Amnesty International är en global organisation som kämpar för alla människors lika värde. Vi arbetar för att mänskliga rättigheter ska gälla alla, nu och i framtiden.", "10 till 20"),
                     new Company(null, "Rädda barnen", "Gustavslundsvägen 141, 107 88 Stockholm","Stockholm", "Alia Bhatt", "Vi finns på plats i Sverige och 119 andra länder och arbetar för alla barns självklara rätt att överleva, utvecklas och växa upp i trygghet. Att rädda barnen är att rädda världen.", "09 till 15"),
@@ -55,7 +55,7 @@ public class CompanyController {
 
     @PostMapping("/postcompany")
     Company postCompany(@RequestBody Map<String, Object> companyForm){
-        return companyRepository.save(new Company(null, (String) companyForm.get("cName"),(String) companyForm.get("cAddress"), (String) companyForm.get("cPerson"),(String) companyForm.get("cDescription"),(String) companyForm.get("cHours")));
+        return companyRepository.save(new Company(null, (String) companyForm.get("cName"),(String) companyForm.get("cAddress"), ((String) companyForm.get("cCity")).trim(),(String) companyForm.get("cPerson"),(String) companyForm.get("cDescription"),(String) companyForm.get("cHours")));
     }
 
     @PostMapping("/logincompany")
