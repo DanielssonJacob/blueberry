bf655cde7366fc2760be68c71cea18af8958ba35
frykj <fryk89@gmail.com>
2022-03-28 11:38:46 +0200

    Blogposts Entity

--- blueberryBackEnd/src/main/java/com/blueberry/blueberry/BlogPosts.java
+++ blueberryBackEnd/src/main/java/com/blueberry/blueberry/BlogPosts.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -18,5 +19,11 @@ public class BlogPosts {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Lob
     private String post;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
 }
