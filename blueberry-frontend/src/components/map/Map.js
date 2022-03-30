d7371ed8117773c4db4a67716633299ed8a7e6f6
potemkin <tom.alpsten@gmail.com>
2022-03-30 14:12:01 +0200

    Marker update

--- blueberry-frontend/src/components/map/Map.js
+++ blueberry-frontend/src/components/map/Map.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import './map.css'
-//import { Icon } from '@iconify/react'
-//import BsFillCaretDownFill from "react-icons/bs"
+//import { Icon } from '@iconify/react';
+//import locationIcon from '@iconify/icons-mdi/map-marker';
+import { MdTerrain } from "react-icons/md";
+import {RiMapPin2Fill} from "react-icons/ri"
 
 
 
 const location = {
-    address: '1600 Amphitheatre Parkway, Mountain View, california.',
-    lat: 37.42216,
-    lng: -122.08427,
+    address: 'Röda korset',
+    lat: 59.32429193804371,
+    lng: 18.06285500502244,
   }
 
   const Map = ({ location, zoomLevel }) => (
@@ -18,21 +20,25 @@ const location = {
   
       <div className="google-map">
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_MY_MAPS_API_KEY}}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_MY_MAPS_API_KEY }}
           defaultCenter={location}
           defaultZoom={zoomLevel}
         >
-          
+          <LocationPin
+            lat={location.lat}
+            lng={location.lng}
+            text={location.address}
+          />
         </GoogleMapReact>
       </div>
     </div>
   )
-
-  /*const LocationPin = ({ text }) => (
+  const LocationPin = ({ text }) => (
     <div className="pin">
-      <Icon icon={BsFillCaretDownFill} className="pin-icon" />
-      <p className="pin-text">{text}</p>
+      <RiMapPin2Fill className="pin-icon" />
+      //<p className="pin-text">{text}</p>
     </div>
-  )*/
+  )
+  
   
   export default Map
